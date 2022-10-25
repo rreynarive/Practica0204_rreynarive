37927e6c5f21ecf526c484ec6552e04edc01ddff
Rosa <rreynarive1@educacion.navarra.es>
2022-10-25 16:53:26 +0200

    Ejercicio 7 FIN

--- Ejercicio 7.py
+++ Ejercicio 7.py
@@ -1,2 +1,10 @@
+# Escribir un programa que pida al usuario un número entero
+# y muestre por pantalla un triángulo rectángulo como el de más abajo,
+# de altura el número introducido.
 
-numero = input("Introduzca un numero ")
+numero = int(input("Introduzca un numero\n"))
+
+for fila in range(numero+1):
+    for a in range(fila):
+        print("*", end=" ")
+    print(" ")
