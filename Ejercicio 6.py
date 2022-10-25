1960e2a325bd098e9cd7f34a5babc62d41b5f467
Rosa <rreynarive1@educacion.navarra.es>
2022-10-25 16:38:31 +0200

    Ejercicio 6 FIN

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -2,4 +2,9 @@
 # y muestre por pantalla todos los años que ha cumplido
 # (desde 1 hasta su edad).
 
+edad = int(input("Introduzca su edad:\n"))
+for i in range(edad):
+    print(i+1, end=" años\n")
+
+
 
