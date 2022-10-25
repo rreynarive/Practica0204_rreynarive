3a6b9603528ce3d46f392fa64948a40d166ef6f3
Rosa <rreynarive1@educacion.navarra.es>
2022-10-25 17:22:41 +0200

    Falta acabar el 8

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -1,2 +1,8 @@
 # Escribir un programa que muestre por
 # pantalla la tabla de multiplicar del 1 al 10.
+
+
+for fila in range(0, 100, 10):
+    for i in range(10+1):
+        print("10 x", i, "=", fila, end="\n")
+print(" ")
