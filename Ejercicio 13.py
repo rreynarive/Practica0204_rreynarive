0a44e3b56887ff94454d299f7032f5aed562ad5b
Rosa <rreynarive1@educacion.navarra.es>
2022-10-26 19:40:59 +0200

    FIN

--- Ejercicio 13.py
+++ Ejercicio 13.py
@@ -1,3 +1,10 @@
 # Escribir un programa que muestre el eco de todo lo que el usuario
 # introduzca hasta que el
 # usuario escriba “salir” que terminará.
+
+while True:
+    mensaje = input("Introduzca su mensaje:\n")
+    if mensaje == "salir":
+        break
+    else:
+        print(mensaje)
